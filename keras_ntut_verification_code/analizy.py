5027c21285f49305e1447d6f346aa17c6027e330
神崎秀吉 <kamisakihideyoshi@outlook.com>
2019-05-25 18:36:19 +0900

    Update training model

--- keras_ntut_verification_code/analizy.py
+++ keras_ntut_verification_code/analizy.py
@@ -1,31 +1,33 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout, Flatten
-from keras.layers import Conv1D
-from keras.layers import MaxPooling1D
+from keras.layers import Conv1D, Conv2D
+from keras.layers import MaxPooling1D, MaxPooling2D
 from sklearn.model_selection import train_test_split
 
 import keras
 import pandas as pd
 import numpy as np
 import cv2
+import string
 
-shape = (40, 40)
-class_num = 26
+shape = (40, 40, 1)
+class_num = len(string.ascii_uppercase)
 
 # 強制 numpy 顯示完整的陣列
-# np.set_printoptions(threshold=np.inf)
+np.set_printoptions(threshold=np.inf)
 
 
-def build(model):
+def build_old(model):
     '''建立模型'''
-    model.add(Conv1D(40, 3, input_shape=shape, padding='same'))
+    model.add(Conv2D(40, (4, 4), input_shape=shape, padding='same'))
     model.add(Activation('relu'))
-    model.add(Conv1D(40, 3, input_shape=shape, padding='same'))
+    model.add(Conv2D(40, (4, 4), padding='same'))
     model.add(Activation('relu'))
-    model.add(MaxPooling1D(pool_size=2))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
-    model.add(Conv1D(80, 3, input_shape=shape, padding='same'))
+    model.add(Conv2D(80, (4, 4), padding='same'))
     model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.5))
     model.add(Flatten())
     model.add(Dense(512))
@@ -36,13 +38,41 @@ def build(model):
     model.add(Activation('softmax'))
     return model
 
+def build(model):
+    '''建立模型'''
+    model.add(MaxPooling2D(pool_size=(2, 2), input_shape=shape))
+    model.add(Conv2D(40, (4, 4), padding='same'))
+    model.add(Activation('relu'))
+    model.add(Conv2D(40, (4, 4), padding='same'))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Conv2D(80, (4, 4), padding='same'))
+    model.add(Activation('relu'))
+    # model.add(Conv2D(40, (2, 2), padding='same'))
+    # model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dropout(0.5))
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dense(64))
+    model.add(Activation('relu'))
+    model.add(Dense(class_num))
+    model.add(Activation('softmax'))
+    model.summary()
+    return model
+
 
 def read_image(path):
     '''從指定路徑讀取圖片
     path -- 圖片的路徑 (包含副檔名)
     '''
     im = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-    # 將圖片二值化
+    # im = im.astype('float64')
+    # im = cv2.resize(im, shape[:2], interpolation=cv2.INTER_LINEAR)
+    im = im.reshape(im.shape[0], im.shape[1], 1)
+    # 將圖片二值化xxx
     im //= 255
     return im
 
@@ -60,6 +90,7 @@ def read_data(csv: str):
     # 將 label 轉為數字
     label = [ord(x)-65 for x in target]
     label = np.asarray(label)
+    # print(np.transpose(np.vstack((target, label))))
     return feature, label
 
 
@@ -75,7 +106,12 @@ def train(model, feature, label, epochs):
     model.compile(loss='categorical_crossentropy',
                   optimizer='adam', metrics=['accuracy'])
     model.fit(train_feature, train_label,
-              batch_size=300, epochs=epochs, verbose=1)
+              batch_size=300, epochs=epochs, verbose=1, validation_split=0.2)
+
+    while input('One more?') == '':
+        model.fit(train_feature, train_label,
+              batch_size=300, epochs=1, verbose=1)
+
     result = model.evaluate(test_feature, test_label, verbose=1)
     print('Test lost:', result[0], ', accuracy:', result[1])
     return model
@@ -85,7 +121,8 @@ def main():
     feature, label = read_data('path.csv')
     model = Sequential()
     model = build(model)
-    train(model, feature, label, epochs=20)
+    model = train(model, feature, label, epochs=10)
+    model.save('nportal_splitted.h5')
 
 
 if __name__ == '__main__':
